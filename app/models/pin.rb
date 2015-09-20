1a62be47403ab1da9b047266a59f47e22645a5df
dayvedde <dayvedde@gmail.com>
2015-09-19 22:30:46 -0500

    validate pins, added relation between user and ppin

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,2 +1,8 @@
 class Pin < ActiveRecord::Base
+	belongs_to :user
+
+	validates :latitude, presence: true
+	validates :longitude, presence:true
+	validates :description, length: {maximum: 100}
+	validates :title, length{maximum: 20}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+	# has many relation with pins
+	has_many :pins
+
 	validates :password, presence: true,
 			  length: {minimum: 8}
 	validates :email, presence: true
