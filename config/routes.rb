283c5ed5921ec00d2fd193da6158494dc2fa8272
dayvedde <dayvedde@gmail.com>
2015-09-20 13:38:28 -0500

    Made users page as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :pins
   resources :users
-  match 'home' => 'users#index' , via:[:get, :post]
+  #match 'home' => 'users#index' , via:[:get, :post]
+  root 'users#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
