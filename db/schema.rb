ece1903f78c4be011304ea5af5593f31618692b8
pcvyas <pcvyas@uwaterloo.ca>
2015-09-22 22:23:50 -0400

    testing listeners

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150920132544) do
+ActiveRecord::Schema.define(version: 20150920032256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "locations", force: :cascade do |t|
-    t.string   "address"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "pins", force: :cascade do |t|
     t.string   "title"
@@ -36,12 +28,11 @@ ActiveRecord::Schema.define(version: 20150920132544) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "password_digest"
+    t.string   "password"
     t.string   "email"
     t.string   "phone_number"
-    t.integer  "pin_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
