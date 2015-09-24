dbe32a1768069745e991244026123a9fddf9bb04
Anakha Palissery <apalissery@gmail.com>
2015-09-24 17:12:19 -0400

    users.scss file updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150920132544) do
+ActiveRecord::Schema.define(version: 20150920032256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "locations", force: :cascade do |t|
-    t.string   "address"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "pins", force: :cascade do |t|
     t.string   "title"
