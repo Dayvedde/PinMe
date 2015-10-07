65d139984be33ce0e8ed10533c6e2310884c387b
dayvedde <dayvedde@gmail.com>
2015-10-06 20:31:40 -0500

    Changed twilio voice message

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -7,7 +7,7 @@ class TwilioController < ApplicationController
 
 	def voice
 		response = Twilio::TwiML::Response.new do |r|
-			r.Say 'Hey There', :voice => 'alice'
+			r.Say 'Hey Anakha, I need to tell you something, shabalaba ding dong beep beep bop poo puu. I like to dig a hole and pretend I am a carrot bye bye', :voice => 'alice'
 			r.Play 'http://linode.rabasa.com/cantina.mp3'
 		end
 		render_twiml response
