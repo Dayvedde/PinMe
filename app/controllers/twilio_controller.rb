116f13f1c1fc530237832675c2f74b10866964ae
dayvedde <dayvedde@gmail.com>
2015-10-06 20:23:27 -0500

    Play song when calling to voice

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -8,6 +8,7 @@ class TwilioController < ApplicationController
 	def voice
 		response = Twilio::TwiML::Response.new do |r|
 			r.Say 'Hey There', :voice => 'alice'
+			r.Play 'http://linode.rabasa.com/cantina.mp3'
 		end
 		render_twiml response
 	end
